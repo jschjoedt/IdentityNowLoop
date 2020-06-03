ccf909334a2601e0453e35d1760f6df912288dfe
JSCH <JSCH@LAPTOP-5MIVD5TV.invixo.local>
2020-06-03 15:19:21 +0200

    Initial commit

---
+++ IdentityNowLoop/src/dk/radius/identitynow/servlet/Main.java
@@ -0,0 +1,113 @@
+package dk.radius.identitynow.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Main
+ */
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set content type
+		response.setContentType("application/xml");
+		
+		// Create dummy xml response
+		
+		
+		response.getWriter()
+			.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+			.append("<Employees>")
+				.append("<Employee>")
+					.append("<Identifier>")
+					.append("12345")
+					.append("</Identifier>")
+
+					.append("<FirstName>")
+					.append("Testy")
+					.append("</FirstName>")
+
+					.append("<LastName>")
+					.append("McTest Face")
+					.append("</LastName>")
+					
+					.append("<DisplayName>")
+					.append("Testy McTest Face")
+					.append("</DisplayName>")
+					
+					.append("<EmailAddress>")
+					.append("testy.mctestface@radius.dk")
+					.append("</EmailAddress>")
+					
+					.append("<ManagerUniqueIdentifier>")
+					.append("9999999")
+					.append("</ManagerUniqueIdentifier>")
+					
+					.append("<Status>")
+					.append("Active")
+					.append("</Status>")
+					
+					.append("<StartDate>")
+					.append("03-06-2020")
+					.append("</StartDate>")
+					
+					.append("<EndDate>")
+					.append("03-06-2020")
+					.append("</EndDate>")
+					
+				.append("</Employee>")						
+					.append("<Employee>")
+					.append("<Identifier>")
+					.append("54321")
+					.append("</Identifier>")
+	
+					.append("<FirstName>")
+					.append("Hello")
+					.append("</FirstName>")
+	
+					.append("<LastName>")
+					.append("Kitty")
+					.append("</LastName>")
+					
+					.append("<DisplayName>")
+					.append("Hello Kitty")
+					.append("</DisplayName>")
+					
+					.append("<EmailAddress>")
+					.append("hello.kitty@radius.dk")
+					.append("</EmailAddress>")
+					
+					.append("<ManagerUniqueIdentifier>")
+					.append("9999999")
+					.append("</ManagerUniqueIdentifier>")
+					
+					.append("<Status>")
+					.append("Inactive")
+					.append("</Status>")
+					
+					.append("<StartDate>")
+					.append("03-06-2020")
+					.append("</StartDate>")
+					
+					.append("<EndDate>")
+					.append("03-06-2025")
+					.append("</EndDate>")
+				.append("</Employee>")	
+			.append("</Employees>");
+	}
+}
