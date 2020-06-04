25636652ddc58bf37f91d7ce7f1984de90767598
XJEHJ <XJEHJ@PC81285.de-prod.dk>
2020-06-04 13:06:49 +0200

    Refactoring and JDocs

--- IdentityNowLoop/src/dk/radius/identitynow/csv/parser/Parser.java
+++ IdentityNowLoop/src/dk/radius/identitynow/csv/parser/Parser.java
@@ -9,39 +9,134 @@ import java.util.ArrayList;
 import dk.radius.identitynow.pojo.Employee;
 
 public class Parser {
-
-	public static ArrayList<Employee> extractEmployees(InputStream is) throws IOException {
-		
+	
+	/**
+	 * Extract employee data from CSV test data and map to Employee pojo.
+	 * @param is Test data input
+	 * @return ArrayList<Employees> with list of Employee objects found in test data
+	 * @throws IOException
+	 */
+	private static ArrayList<Employee> extractEmployees(InputStream is) throws IOException {
 		ArrayList<Employee> empList = new ArrayList<Employee>();
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String line = "";
-        String cvsSplitBy = ";";
 
-        
-            while ((line = br.readLine()) != null) {
-            	String[] lineData = line.split(cvsSplitBy);
-            	
-            	Employee emp = new Employee();
-            	
-            	emp.identifier = lineData[0].trim();
-            	emp.managerId = lineData[1].trim();
-            	emp.firstName = lineData[3].trim();
-            	emp.lastName = lineData[4].trim();
-            	emp.displayName = emp.lastName + ", " + emp.firstName;
-            	emp.status = lineData[5].trim();
-            	emp.startDate = lineData[6].trim();
-            	emp.endDate = lineData[7].trim();
-            	emp.type = lineData[9].trim();
-            	emp.email = lineData[10].trim().toLowerCase();
-            	
-            	empList.add(emp);
-            	
-            }
-            
-            br.close();
-            
-            return empList;
+		// Read test data stream
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line = "";
+		String cvsSplitBy = ";";
+
+		// Process csv lines
+		while ((line = br.readLine()) != null) {
+			String[] lineData = line.split(cvsSplitBy);
+
+			// Create employee for each new line
+			Employee emp = new Employee();
+			emp.identifier = lineData[0].trim();
+			emp.managerId = lineData[1].trim();
+			emp.firstName = lineData[3].trim();
+			emp.lastName = lineData[4].trim();
+			emp.displayName = emp.lastName + ", " + emp.firstName;
+			emp.status = lineData[5].trim();
+			emp.startDate = lineData[6].trim();
+			emp.endDate = lineData[7].trim();
+			emp.type = lineData[9].trim();
+			emp.email = lineData[10].trim().toLowerCase();
+
+			// Add to list
+			empList.add(emp);
+		}
+		
+		// Done reading
+		br.close();
+		
+		// Return list of loaded Employees
+		return empList;
 	}
+	
+
+	/**
+	 * Build an XML string based on a list of employees.
+	 * @param empList ArrayList<Employee> containing employee objects with data
+	 * @return <i>String</i>: XML representation of employee data to match SAP ECC format
+	 */
+	private static String buildEmployeeResponse(ArrayList<Employee> empList) {
+		StringBuilder sb = new StringBuilder();
+
+		// Add XML start of response
+		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+		.append("<ns0:EmployeesResponse xmlns:ns0=\"http://radiuselnet.dk/decs/identitynow\">")
+		.append("<Employees>");
+
+		// Process and add each employee in list
+		for (Employee emp : empList) {
+			sb.append("<Employee>")
+			.append("<Identifier>")
+			.append(emp.identifier)
+			.append("</Identifier>")
+
+			.append("<Type>")
+			.append(emp.type)
+			.append("</Type>")
+
+			.append("<FirstName>")
+			.append(emp.firstName)
+			.append("</FirstName>")
+
+			.append("<LastName>")
+			.append(emp.lastName)
+			.append("</LastName>")
+
+			.append("<DisplayName>")
+			.append(emp.displayName)
+			.append("</DisplayName>")
+
+			.append("<EmailAddress>")
+			.append(emp.email)
+			.append("</EmailAddress>")
+
+			.append("<ManagerUniqueIdentifier>")
+			.append(emp.managerId)
+			.append("</ManagerUniqueIdentifier>")
+
+			.append("<Status>")
+			.append(emp.status)
+			.append("</Status>")
+
+			.append("<StartDate>")
+			.append(emp.startDate)
+			.append("</StartDate>")
+
+			.append("<EndDate>")
+			.append(emp.endDate)
+			.append("</EndDate>")	
+			.append("</Employee>");		
+		}
+
+		// finalize XML response
+		sb.append("</Employees>")
+		.append("</ns0:EmployeesResponse>");
+
+		// Return full response
+		return sb.toString();
+	}
+
+	
+	/**
+	 * Read and parse CSV test data.
+	 * @param is Test data input
+	 * @return String XML representation of expected SAP ECC data
+	 * @throws IOException
+	 */
+	public static String processEmployeeData(InputStream is) throws IOException {
+		// Extract employees from csv file
+		ArrayList<Employee> empList = extractEmployees(is);
+
+		// Create XML string from employee list
+		String response = buildEmployeeResponse(empList);
+
+		// Return XML response
+		return response;
+	}
+
+
 
 }
--- IdentityNowLoop/src/dk/radius/identitynow/servlet/Main.java
+++ IdentityNowLoop/src/dk/radius/identitynow/servlet/Main.java
@@ -2,15 +2,12 @@ package dk.radius.identitynow.servlet;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dk.radius.identitynow.csv.parser.Parser;
-import dk.radius.identitynow.pojo.Employee;
 
 /**
  * Servlet implementation class Main
@@ -23,81 +20,22 @@ public class Main extends HttpServlet {
      */
     public Main() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		// Set content type
 		response.setContentType(request.getContentType());
 		
 		// Load test data from local file in project
 		InputStream is = getServletContext().getResourceAsStream("/TestData/Test_extract_csv.csv");
 		
-		// Extract employees from csv file
-		ArrayList<Employee> empList = Parser.extractEmployees(is);
-				
-				
-		String employeesResponse = buildEmployeeResponse(empList);
+		// Process local test data
+		String employeesResponse = Parser.processEmployeeData(is);
 		
+		// Set response
 		response.getWriter().append(employeesResponse);
-	}
-
-	private String buildEmployeeResponse(ArrayList<Employee> empList) {
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
-		.append("<ns0:EmployeesResponse xmlns:ns0=\"http://radiuselnet.dk/decs/identitynow\">")
-		.append("<Employees>");
-		for (Employee emp : empList) {
-			sb.append("<Employee>")
-			.append("<Identifier>")
-			.append(emp.identifier)
-			.append("</Identifier>")
-
-			.append("<Type>")
-			.append(emp.type)
-			.append("</Type>")
-			
-			.append("<FirstName>")
-			.append(emp.firstName)
-			.append("</FirstName>")
-
-			.append("<LastName>")
-			.append(emp.lastName)
-			.append("</LastName>")
-			
-			.append("<DisplayName>")
-			.append(emp.displayName)
-			.append("</DisplayName>")
-			
-			.append("<EmailAddress>")
-			.append(emp.email)
-			.append("</EmailAddress>")
-			
-			.append("<ManagerUniqueIdentifier>")
-			.append(emp.managerId)
-			.append("</ManagerUniqueIdentifier>")
-			
-			.append("<Status>")
-			.append(emp.status)
-			.append("</Status>")
-			
-			.append("<StartDate>")
-			.append(emp.startDate)
-			.append("</StartDate>")
-			
-			.append("<EndDate>")
-			.append(emp.endDate)
-			.append("</EndDate>")	
-		.append("</Employee>");		
-		}
-		sb.append("</Employees>")
-		.append("</ns0:EmployeesResponse>");
-		
-		return sb.toString();
 	}
 }
